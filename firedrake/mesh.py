dbc7cd4591c7eb95b48c76e03bd40599fba67aae
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-04-23 12:08:03 +0100

    Store permutation from new to original cell numbering
    We need this to back out the correct coarse to fine cell maps in a mesh
    hierarchy.

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -304,7 +304,7 @@ class Mesh(object):
 
         self._ufl_domain = ufl.Domain(self.ufl_cell(), data=self)
         dim = self._plex.getDimension()
-        self._cells, self.cell_classes = dmplex.get_cells_by_class(self._plex)
+        self._cells, self.cell_classes, self._inv_cells = dmplex.get_cells_by_class(self._plex)
 
         # Derive a cell numbering from the Plex renumbering
         cell_entity_dofs = np.zeros(dim+1, dtype=np.int32)
