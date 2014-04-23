4ca74224a5e8691ae0da17bffb898980ac27cb2d
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-04-23 14:56:10 +0100

    Add basic test of restriction/prolongation
    Restrict and prolong a DG0 field with handwritten kernels.

---
+++ tests/regression/test_multigrid_basics.py
@@ -0,0 +1,47 @@
+from firedrake import *
+import pytest
+import numpy as np
+
+
+@pytest.fixture(scope='module')
+def g():
+    mesh = UnitSquareMesh(1, 1)
+    mh = MeshHierarchy(mesh, 3)
+    fs = FunctionSpaceHierarchy(mh, 'DG', 0)
+
+    return FunctionHierarchy(fs)
+
+
+def test_prolongation_dg0(g):
+    for f in g:
+        f.assign(0)
+    g[0].assign(1)
+
+    for f in g[1:]:
+        assert np.allclose(f.dat.data, 0)
+
+    for i in range(len(g) - 1):
+        g.prolong(i)
+
+    for f in g:
+        assert np.allclose(f.dat.data, 1)
+
+
+def test_restriction_dg0(g):
+    for f in g:
+        f.assign(0)
+    g[-1].assign(1)
+
+    for f in g[:-1]:
+        assert np.allclose(f.dat.data, 0)
+
+    for i in range(len(g) - 1, 0, -1):
+        g.restrict(i)
+
+    for f in g:
+        assert np.allclose(f.dat.data, 1)
+
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
