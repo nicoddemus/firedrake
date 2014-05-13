66db2d580001b0293d5f8fcbbcc7e1d824ea1479
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-05-13 19:04:25 +0100

    Add failing test restricting interpolate coordinates
    Fails because inverse cell map is wrong.

--- tests/regression/test_multigrid_basics.py
+++ tests/regression/test_multigrid_basics.py
@@ -42,6 +42,21 @@ def test_restriction_dg0(g):
         assert np.allclose(f.dat.data, 1)
 
 
+@pytest.mark.xfail(reason="Inverse cell map not correct")
+def test_restriction_dg0_x(g):
+    for f in g:
+        f.assign(0)
+    g[-1].interpolate(Expression("x[0]"))
+
+    for i in range(len(g) - 1, 0, -1):
+        g.restrict(i)
+
+    for f in g:
+        tmp = Function(f)
+        tmp.interpolate(Expression("x[0]"))
+        assert np.allclose(tmp.dat.data, f.dat.data)
+
+
 if __name__ == '__main__':
     import os
     pytest.main(os.path.abspath(__file__))
